332986d3056b3de83a4507903ba839b1d08d26d2
Owen Gretzinger <owengretzinger@gmail.com>
2025-02-16 21:23:22 -0500

    fix bug with loading existing readmes

--- nextjs/src/app/[[...repoPath]]/page.tsx
+++ nextjs/src/app/[[...repoPath]]/page.tsx
@@ -6,6 +6,7 @@ import { useReadme } from "./use-readme";
 import ViewReadme from "./view-readme";
 import { useEffect } from "react";
 import { Toaster } from "~/components/ui/toaster";
+import { GenerationState } from "./use-readme-stream";
 
 export default function Readme() {
   const params = useParams<{ repoPath: string | undefined }>();
@@ -13,20 +14,35 @@ export default function Readme() {
   const repoPathFromUrl = Array.isArray(params.repoPath)
     ? params.repoPath.join("/")
     : params.repoPath!;
+  const versionFromUrl = searchParams.get("v");
 
   const readmeGenerator = useReadme();
 
+  // logic for loading existing readme from db based on the url
   useEffect(() => {
-    if (repoPathFromUrl && !readmeGenerator.repoUrl) {
-      // we need to set all the readme info instead of just the url
-      readmeGenerator.setRepoUrlFromPath(repoPathFromUrl);
-      // Set version from URL if it exists, otherwise let the hook handle getting latest version
-      const versionFromUrl = searchParams.get("v");
-      if (versionFromUrl) {
-        readmeGenerator.setVersion(parseInt(versionFromUrl));
+    if (repoPathFromUrl) {
+      // currentRepoPath is the cached repo path
+      const currentRepoPath = readmeGenerator.repoUrl?.split('github.com/')[1];
+      
+      // Check if we need to update anything
+      const needsUrlUpdate = currentRepoPath !== repoPathFromUrl;
+      const needsVersionUpdate = versionFromUrl && parseInt(versionFromUrl) !== readmeGenerator.version;
+      
+      if (needsUrlUpdate || needsVersionUpdate) {
+        // Reset state if URL is changing
+        if (needsUrlUpdate) {
+          readmeGenerator.setReadmeContent("");
+          readmeGenerator.setReadmeGenerationState(GenerationState.NOT_STARTED);
+          readmeGenerator.setRepoUrl(`https://github.com/${repoPathFromUrl}`);
+        }
+        
+        // Update version if needed
+        if (needsVersionUpdate) {
+          readmeGenerator.setVersion(parseInt(versionFromUrl));
+        }
       }
     }
-  }, [repoPathFromUrl, readmeGenerator.repoUrl, readmeGenerator, searchParams]);
+  }, [repoPathFromUrl, readmeGenerator, versionFromUrl]);
 
   useEffect(() => {
     console.log("repoPathFromUrl: ", repoPathFromUrl);
