9ca40e50f7d1c3dc559e7379097178a73892474d
Owen Gretzinger <owengretzinger@gmail.com>
2025-03-02 23:52:21 -0500

    add tutorial video

--- nextjs/src/lib/links.ts
+++ nextjs/src/lib/links.ts
@@ -1,2 +1,2 @@
-export const tutorialLink = "https://example.com";
+export const tutorialLink = "https://www.youtube.com/embed/GCaKJDEK_3M?si=pK1A1Hm6dGMdx5VD";
 export const githubLink = "https://github.com/owengretzinger/gitreadme";
